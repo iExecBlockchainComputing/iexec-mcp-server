b92a8ae25387d76bab151e2e530030ee96ab4cd0
paypes <benayache.abbes@gmail.com>
2025-05-29 01:21:33 +0200

    refactor fetchMyContact

--- src/tools/web3mail/fetchMyContacts.ts
+++ src/tools/web3mail/fetchMyContacts.ts
@@ -1,4 +1,4 @@
-import { getWeb3Provider, IExecWeb3mail } from "@iexec/web3mail";
+import { getWeb3Provider, IExecWeb3mail, WorkflowError } from "@iexec/web3mail";
 import { McpError, ErrorCode } from "@modelcontextprotocol/sdk/types.js";
 import { readWalletPrivateKey } from "../../utils/readWalletKeystore.js";
 
@@ -15,9 +15,21 @@ export const fetchMyContacts = {
             const privateKey = await readWalletPrivateKey();
             const web3Provider = getWeb3Provider(privateKey);
             const web3mail = new IExecWeb3mail(web3Provider);
-            return await web3mail.fetchMyContacts();
+            const contacts = await web3mail.fetchMyContacts();
+            return {
+                message: "Contacts fetched successfully",
+                contacts
+            };
         } catch (error: any) {
+            if (error instanceof WorkflowError) {
+                if (error.isProtocolError) {
+                    throw new McpError(ErrorCode.InternalError, error.message);
+                } else {
+                    throw new McpError(ErrorCode.InvalidParams, error.message);
+                }
+            }
             throw new McpError(ErrorCode.InternalError, error.message);
+
         }
     },
 };
